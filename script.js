b4f3b9b72b33b1bee84af0b8706ebbb6d6c3b291
João Costa <joaoluissaraivacosta@gmail.com>
2023-06-10 15:39:50 +0100

    calculator interface done!

---
+++ script.js
@@ -0,0 +1,15 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+  if (isNaN(value)) {
+    // this is not a number
+    handleSymbol(value);
+  } else {
+    // this is a number
+    handleNumber(value);
+  }
+}
